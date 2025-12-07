821306a10a76d105754eb1d572cfbbd8d7e47274
evdokimov-dens <evdokimov.denis96@yandex.ru>
2025-12-08 00:16:07 +0500

    ended: img blur project

--- js/main.js
+++ js/main.js
@@ -1,10 +1,14 @@
 window.onload = init;
 function init() {
-  let image = document.getElementById("zero");
-  image.onclick = showAnswer;
+  let images = document.getElementsByTagName("img");
+  for (i = 0; i < images.length; i++) {
+    images[i].onclick = showAnswer;
+  }
 }
 
-function showAnswer() {
-  let image = document.getElementById("zero");
-  image.src = "/img/zero.jpg";
+function showAnswer(eventObj) {
+  let image = eventObj.target;
+  let name = image.id;
+  name = "/img/" + name + ".jpg";
+  image.src = name;
 }
